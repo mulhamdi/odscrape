782c7bf76eb56d64bd10c1d259c46973ff68c4eb
Mimu <124642469+mulhamdi@users.noreply.github.com>
2024-06-27 12:01:06 +0700

    Fetch anime data
    Fetchin data rilisan anime terbaru dan menampilkannya ke konsol untuk tujuan pengetesan.

---
+++ main.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+// Global variables
+const TARGET_URL = 'https://otakudesu.cloud';
+
+const axiosParams = {
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+  },
+};
+
+async function getHTMLSource(URL) {
+  try {
+    const response = await axios.get(URL, axiosParams);
+    const html = response.data;
+    return html;
+  } catch (error) {
+    if (error.response) {
+      console.error('The request was made and the server responded:');
+      console.error(error.response.data);
+      console.error(error.response.status);
+      console.error(error.response.headers);
+    } else if (error.request) {
+      console.error('The request was made but no response was recieved:');
+      console.error(error.request);
+    } else {
+      console.error('Error', error.message);
+    }
+    console.error(error.config);
+  }
+}
+
+async function getRedirectedURL(URL) {
+  return await axios
+    .get(URL)
+    .then((response) => response.request.res.responseUrl);
+}
+
+async function getStreamURL(animePageURL) {
+  const animePage = await getHTMLSource(animePageURL);
+  const $animePage = cheerio.load(animePage);
+  const latestEpsURL = $animePage('div.episodelist')
+    .find('a')
+    .first()
+    .attr('href');
+
+  const streamPage = await getHTMLSource(latestEpsURL);
+  const $streamPage = cheerio.load(streamPage);
+  let pixelDrainLink = $streamPage('div.download')
+    .find('strong:contains("MKV 1080p")')
+    .next()
+    .next()
+    .attr('href');
+  pixelDrainLink = await getRedirectedURL(pixelDrainLink);
+
+  return pixelDrainLink;
+}
+
+async function getAnimeData() {
+  try {
+    const mainPage = await getHTMLSource(TARGET_URL);
+    const $mainPage = cheerio.load(mainPage);
+    const firstAnimesContainer = $mainPage('.venz').first().find('li');
+    let i = 0;
+    for (const card of firstAnimesContainer) {
+      if (i === 1) break;
+      i++;
+      console.log(`\n${i}.\n----`);
+      const animeInfo = {
+        title: $mainPage(card).find('h2.jdlflm').text().trim(),
+        thumbnail: $mainPage(card).find('img').attr('src'),
+        episode: $mainPage(card).find('div.epz').text().trim(),
+        releaseDay: $mainPage(card).find('div.epztipe').text().trim(),
+        releaseDate: $mainPage(card).find('div.newnime').text(),
+        streamURL: await getStreamURL($mainPage(card).find('a').attr('href')),
+      };
+      for (const data in animeInfo) {
+        console.log(animeInfo[data]);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getAnimeData();
