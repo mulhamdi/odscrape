ce40a23b80b899f2fa0a60b1805ba783e36a166e
Mimu <124642469+mulhamdi@users.noreply.github.com>
2024-06-30 14:30:52 +0700

    Custom HTTP Header
    Menambahkan custom HTTP header agar browser-like request.

--- main.js
+++ main.js
@@ -10,7 +10,12 @@ const axiosParams = {
   headers: {
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    Accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
   },
+  withCredentials: true,
 };
 
 async function getHTMLSource(URL) {
